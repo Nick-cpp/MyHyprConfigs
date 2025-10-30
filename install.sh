7cfe5b304337247cce365a98fc1e1dc3224b0a02
NikolaTeslaProgrammist <gnots.nik@gmail.com>
2025-10-30 12:52:46 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,9 @@ git clone https://github.com/Nick-cpp/MyHyprConfigs
 
 mkdir ~/Pictures
 mkdir ~/Pictures/Wallpapers
-sleep 1
 cp -rf ~/MyHyprConfigs/Wallpapers ~/Pictures/Wallpapers/
-sleep 1
 cp ~/MyHyprConfigs/power-menu.sh ~/
-sleep 1
 sudo cp -rf ~/MyHyprConfigs/Mogo-Candy-Black/ /usr/share/icons/
-sleep 1
 chmod +x ~/power-menu.sh
 sleep 2
 cp -rf ~/MyHyprConfigs/configs/* ~/.config/
