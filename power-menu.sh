3bfd6297f3a754006b36bdf07c4376f74953f3e1
NikolaTeslaProgrammist <gnots.nik@gmail.com>
2025-11-02 12:23:03 +0500

    Update power-menu.sh

--- power-menu.sh
+++ power-menu.sh
@@ -5,14 +5,12 @@ choice=$(echo -e "Reboot\nShutdown" | wofi --dmenu --prompt="Power Menu" --heigh
 
 case "$choice" in
     "Reboot")
-        # Перезагрузка
         confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=100 --width=200)
         if [ "$confirm" = "Yes" ]; then
             systemctl reboot
         fi
         ;;
     "Shutdown")
-        # Выключение
         confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=100 --width=200)
         if [ "$confirm" = "Yes" ]; then
             systemctl poweroff
