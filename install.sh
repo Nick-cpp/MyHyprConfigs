bbe3f971a677b61cd96b727e525135316a9f4ace
Mikola 2.0 <gnots.nik@gmail.com>
2025-11-08 14:18:48 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd
 echo installing needed packages
 sleep 2
-sudo pacman -Syu noto-fonts-emoji nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock micro gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
+sudo pacman -Syu hyprpicker noto-fonts-emoji nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock micro gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
 git clone https://github.com/Nick-cpp/MyHyprConfigs
 echo creating ~/Pictures and ~/Pictures/Wallpapers and ~/Pictures/Screenshots directories
 sleep 2
