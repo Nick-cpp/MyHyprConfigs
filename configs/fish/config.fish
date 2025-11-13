33f1af2e5151b3470debb482c67bce6050f1447f
Mikola 2.0 <gnots.nik@gmail.com>
2025-11-13 17:42:02 +0500

    Update config.fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -2,16 +2,20 @@
 #### ALIASES ####
 #################
 
-alias suka="sudo archarchive"
+alias suka="su -c "archarchive""
 alias m="micro"
 alias g="git"
 alias c="clear"
-alias sp="sudo pacman"
 alias fucking="sudo"
 alias nyaofetch="fastfetch"
 alias wmconf="m ~/.config/hypr/hyprland.conf"
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 set -U fish_greeting
+function super
+    su -c "$argv"
+end
+function sp
+    su -c "pacman $argv"
+end
