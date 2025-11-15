b376994cb1b63d7bdef3c0bbab08740551ae60ab
Mikola 2.0 <gnots.nik@gmail.com>
2025-11-15 16:53:22 +0500

    Add files via upload

---
+++ wallpaper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+WALLPAPER=$(find ~/Pictures/Wallpapers/ -type f | shuf -n1)
+
+swww img "$WALLPAPER" --transition-type none
+
+HYPRLOCK_CONFIG="$HOME/.config/hypr/hyprlock.conf"
+
+sed -i "s|path = .*|path = $WALLPAPER|" "$HYPRLOCK_CONFIG"
