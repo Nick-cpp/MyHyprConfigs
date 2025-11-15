07353f57ffd24e9c0be2186b6e900b92c39a1f33
Mikola 2.0 <gnots.nik@gmail.com>
2025-11-15 08:59:03 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd
 echo installing needed packages
 sleep 2
-sudo pacman -Syu hyprpicker noto-fonts-emoji nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock micro gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
+sudo pacman -Syu unzip hyprpicker noto-fonts-emoji nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock micro gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
 echo creating ~/Pictures and ~/Pictures/Wallpapers and ~/Pictures/Screenshots directories
 sleep 2
 mkdir -p ~/Pictures/Wallpapers
@@ -15,7 +15,8 @@ cp ~/MyHyprConfigs/power-menu.sh ~/
 cp ~/MyHyprConfigs/network-menu.sh ~/
 echo copying the cursor theme
 sleep 2
-sudo cp -rf ~/MyHyprConfigs/Moga-Candy-Black/ /usr/share/icons/
+unzip ~/MyHyprConfigs/Moga-Candy-Black.zip
+sudo cp -rf ~/Moga-Candy-Black/Moga-Candy-Black /usr/share/icons/
 chmod +x ~/power-menu.sh
 chmod +x ~/network-menu.sh
 echo copying configs
