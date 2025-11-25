a702e0ee13126592b4f20b41bba8372973b2d9f2
Mikola 2.0 <gnots.nik@gmail.com>
2025-11-25 16:20:29 +0500

    Update config.fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -8,7 +8,7 @@ alias g="git"
 alias c="clear"
 alias fucking="sudo"
 alias nyaofetch="fastfetch"
-alias wmconf="m ~/.config/hypr/hyprland.conf"
+alias wmconf="vim ~/.config/hypr/hyprland.conf"
 alias plant="cbonsai"
 
 if status is-interactive
