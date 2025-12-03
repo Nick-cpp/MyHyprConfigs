beb4ef3a4135b61eff594bb6dd89faf50cf75d0a
Mikola 2.0 <gnots.nik@gmail.com>
2025-12-03 23:43:34 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ cd
 echo installing needed packages
 sleep 2
 sudo pacman -Syu unzip hyprpicker noto-fonts-emoji noto-fonts noto-fonts-cjk nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
-echo recompiling vim with system clipboard enabled
+sudo pacman -R vim
+echo compiling vim with system clipboard enabled
 sleep 2
 git clone https://github.com/vim/vim.git
 cd vim
