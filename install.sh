913e3df4bedd1d3f12d732f43b83665a5fdd9ea1
Mikola 2.0 <gnots.nik@gmail.com>
2025-12-03 23:42:54 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,27 @@
 cd
 echo installing needed packages
 sleep 2
-sudo pacman -Syu gvim unzip hyprpicker noto-fonts-emoji noto-fonts noto-fonts-cjk nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
+sudo pacman -Syu unzip hyprpicker noto-fonts-emoji noto-fonts noto-fonts-cjk nwg-look gthumb haruna imagemagick base-devel hyprland waybar wofi firefox thunar ttf-font-awesome otf-font-awesome ttf-jetbrains-mono fish pkgfile ttf-dejavu powerline-fonts inetutils fastfetch pavucontrol hyprshot hyprlock gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb udisks2 swww git
+echo recompiling vim with system clipboard enabled
+sleep 2
+git clone https://github.com/vim/vim.git
+cd vim
+./configure \
+                       --with-features=huge \
+                       --enable-multibyte \
+                       --enable-python3interp=yes \
+                       --with-python3-command=python \
+                       --enable-cscope \
+                       --enable-terminal \
+                       --enable-autoservername \
+                       --enable-fontset \
+                       --with-wayland \
+                       --enable-gui=no \
+                       --with-x=no
+
+make -j$(nproc)
+sudo make install
+cd
 echo installing vim plug
 sleep 2
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
