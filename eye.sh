60bd2c310be7107e5d7baf234066618c072e9e9d
Mikola 2.0 <gnots.nik@gmail.com>
2025-12-18 21:03:27 +0500

    Refactor eye break notifications for clarity
    Refactor break notifications to format time and improve readability.

--- eye.sh
+++ eye.sh
@@ -17,13 +17,35 @@ for arg in "$@"; do
     esac
 done
 
+format_time() {
+    local seconds=$1
+    local mins=$((seconds / 60))
+    local secs=$((seconds % 60))
+    printf "%02d:%02d" $mins $secs
+}
+
 while true; do
     sleep $((INTERVAL * 60))
     
     touch "$LOCK_FILE"
     
     if [ $SHORT_BREAK_COUNT -lt 2 ]; then
-        hyprctl dispatch exec "[float; size 325 200; center] kitty --title 'Eye Break' --class=eye-break -e bash -c 'printf \"\033[1;31m\"; echo \"========================\"; echo \"     EYE BREAK TIME!\"; echo \"========================\"; printf \"\033[0m\"; echo \"Look away for $DURATION seconds\"; echo \"\"; for i in {$DURATION..1}; do echo -ne \"\\\rTime left: \$i seconds\"; sleep 1; done; echo -e \"\\\rBreak finished!                \"; sleep 2'"
+        hyprctl dispatch exec "[float; size 325 200; center] kitty --title 'Eye Break' --class=eye-break -e bash -c '
+            printf \"\033[1;31m\"
+            echo \"========================\"
+            echo \"     EYE BREAK TIME!\"
+            echo \"========================\"
+            printf \"\033[0m\"
+            echo \"Look away for $DURATION seconds\"
+            echo \"\"
+            for i in {$DURATION..1}; do
+                formatted_time=\$(printf \"%02d:%02d\" \$((i/60)) \$((i%60)))
+                echo -ne \"\\\rTime left: \$formatted_time\"
+                sleep 1
+            done
+            echo -ne \"\\\rBreak finished!                \"
+            sleep 2
+        '"
         SHORT_BREAK_COUNT=$((SHORT_BREAK_COUNT + 1))
         rm -f "$LOCK_FILE"
         sleep $DURATION
@@ -31,12 +53,42 @@ while true; do
         SHORT_BREAK_COUNT=0
         
         if [ $LONG_BREAK_COUNT -eq 0 ]; then
-            hyprctl dispatch exec "[float; size 325 200; center] kitty --title 'Long Eye Break' --class=eye-break -e bash -c 'printf \"\033[1;32m\"; echo \"========================\"; echo \"    LONG BREAK TIME!\"; echo \"========================\"; printf \"\033[0m\"; echo \"Look away for 5 minutes\"; echo \"\"; for i in {300..1}; do echo -ne \"\\\rTime left: \$i seconds\"; sleep 1; done; echo -e \"\\\rBreak finished!                \"; sleep 2'"
+            hyprctl dispatch exec "[float; size 325 200; center] kitty --title 'Long Eye Break' --class=eye-break -e bash -c '
+                printf \"\033[1;32m\"
+                echo \"========================\"
+                echo \"    LONG BREAK TIME!\"
+                echo \"========================\"
+                printf \"\033[0m\"
+                echo \"Look away for 5 minutes\"
+                echo \"\"
+                for i in {300..1}; do
+                    formatted_time=\$(printf \"%02d:%02d\" \$((i/60)) \$((i%60)))
+                    echo -ne \"\\\rTime left: \$formatted_time\"
+                    sleep 1
+                done
+                echo -ne \"\\\rBreak finished!                \"
+                sleep 2
+            '"
             LONG_BREAK_COUNT=1
             rm -f "$LOCK_FILE"
             sleep 300
         else
-            hyprctl dispatch exec "[float; size 325 200; center] kitty --title 'Extra Long Eye Break' --class=eye-break -e bash -c 'printf \"\033[1;34m\"; echo \"========================\"; echo \" EXTRA LONG BREAK TIME!\"; echo \"========================\"; printf \"\033[0m\"; echo \"Look away for 10 minutes\"; echo \"\"; for i in {600..1}; do echo -ne \"\\\rTime left: \$i seconds\"; sleep 1; done; echo -e \"\\\rBreak finished!                \"; sleep 2'"
+            hyprctl dispatch exec "[float; size 325 200; center] kitty --title 'Extra Long Eye Break' --class=eye-break -e bash -c '
+                printf \"\033[1;34m\"
+                echo \"========================\"
+                echo \" EXTRA LONG BREAK TIME!\"
+                echo \"========================\"
+                printf \"\033[0m\"
+                echo \"Look away for 10 minutes\"
+                echo \"\"
+                for i in {600..1}; do
+                    formatted_time=\$(printf \"%02d:%02d\" \$((i/60)) \$((i%60)))
+                    echo -ne \"\\\rTime left: \$formatted_time\"
+                    sleep 1
+                done
+                echo -ne \"\\\rBreak finished!                \"
+                sleep 2
+            '"
             LONG_BREAK_COUNT=0
             rm -f "$LOCK_FILE"
             sleep 600
