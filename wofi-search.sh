7b739634ae1fce54e3bf975ff6f95efa22645d39
Mikola 2.0 <gnots.nik@gmail.com>
2025-12-20 14:36:04 +0500

    Create wofi-search.sh

---
+++ wofi-search.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+query=$(wofi --show dmenu -p "Wiki Search:" --width 250 --height 50)
+
+if [[ -n "$query" ]]; then
+    target=$(echo -e "Arch Wiki\nArtix Wiki" | wofi --show dmenu -i -p "Select Source:" --width 250 --height 100)
+
+    if [[ -z "$target" ]]; then
+        exit 0
+    fi
+
+    urlencode() {
+        local string="${1}"
+        local strlen=${#string}
+        local encoded=""
+        local pos c o
+        
+        for (( pos=0 ; pos<strlen ; pos++ )); do
+            c="${string:$pos:1}"
+            case "$c" in
+                [-_.~a-zA-Z0-9]) 
+                    o="${c}" 
+                    ;;
+                *) 
+                    printf -v o '%%%02X' "'$c"
+                    ;;
+            esac
+            encoded+="${o}"
+        done
+        echo "${encoded}"
+    }
+
+    encoded_query=$(urlencode "$query")
+
+    case "$target" in
+        "Arch Wiki")
+            url="https://wiki.archlinux.org/index.php?search=${encoded_query}"
+            ;;
+        "Artix Wiki")
+            url="https://wiki.artixlinux.org/index.php?action=search&q=${encoded_query}"
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+    
+    xdg-open "$url" > /dev/null 2>&1 & disown
+fi
