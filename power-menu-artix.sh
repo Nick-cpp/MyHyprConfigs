f0ad6bb9d2693a12519915a08b5372ec222e690b
Mikola 2.0 <gnots.nik@gmail.com>
2025-12-22 14:20:47 +0500

    Add files via upload

---
+++ power-menu-artix.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+choice=$(echo -e "Reboot\nShutdown" | wofi --dmenu --prompt="Power Menu" --height=120 --width=250)
+
+case "$choice" in
+    "Reboot")
+        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=100 --width=200)
+        if [ "$confirm" = "Yes" ]; then
+            loginctl reboot
+        fi
+        ;;
+    "Shutdown")
+        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=100 --width=200)
+        if [ "$confirm" = "Yes" ]; then
+            loginctl poweroff
+        fi
+        ;;
+esac
