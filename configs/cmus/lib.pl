29b26b54aa4fb877191448088382f02d7c7527d4
Mikola 2.0 <gnots.nik@gmail.com>
2025-12-27 11:17:33 +0500

    Add files via upload

---
+++ configs/cmus/lib.pl
@@ -0,0 +1,11 @@
+/home/nikolas/Music/Billy Talent - Fallen leaves.mp3
+/home/nikolas/Music/Michael Jackson - Billie Jean.mp3
+/home/nikolas/Music/Алексей Вишня - Шприц.mp3
+/home/nikolas/Music/Анимация - Родина.mp3
+/home/nikolas/Music/БИ2 - Черное солнце.mp3
+/home/nikolas/Music/НТР - Костыль и велосипед.mp3
+/home/nikolas/Music/НТР - Надо было ставить линукс v2.mp3
+/home/nikolas/Music/НТР - Надо было ставить линукс.mp3
+/home/nikolas/Music/НТР - Папа может в си.mp3
+/home/nikolas/Music/Pain - Shout your mouth.mp3
+/home/nikolas/Music/Король и шут - Кукла колдуна.mp3
