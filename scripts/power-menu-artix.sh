6b22da5f2fd5388b104319e41b4ac6231a3dc4ca
Mikola 2.0 <gnots.nik@gmail.com>
2026-01-10 20:26:09 +0500

    Replace killall with pkill for hyprland

--- scripts/power-menu-artix.sh
+++ scripts/power-menu-artix.sh
@@ -6,14 +6,14 @@ case "$choice" in
     "Reboot")
         confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=100 --width=200)
         if [ "$confirm" = "Yes" ]; then
-            killall -w hyprland
+            pkill -15 -x hyprland
             loginctl reboot
         fi
         ;;
     "Shutdown")
         confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=100 --width=200)
         if [ "$confirm" = "Yes" ]; then
-            killall -w hyprland
+            pkill -15 -x hyprland
             loginctl poweroff
         fi
         ;;
