9d6eecdb8bcd6c2da6e7109a1b4722978fb1559b
Mikola 2.0 <gnots.nik@gmail.com>
2026-01-15 17:58:45 +0500

    Update power-menu.sh

--- scripts/power-menu.sh
+++ scripts/power-menu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-choice=$(echo -e "Reboot\nShutdown" | wofi --dmenu --prompt="Power Menu" --height=120 --width=250)
+choice=$(echo -e "Reboot\nShutdown" | wofi --dmenu --prompt="Power Menu" --height=150 --width=250)
 
 case "$choice" in
     "Reboot")
-        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=100 --width=200)
+        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=150 --width=200)
         if [ "$confirm" = "Yes" ]; then
             reboot
         fi
         ;;
     "Shutdown")
-        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=100 --width=200)
+        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=150 --width=200)
         if [ "$confirm" = "Yes" ]; then
             poweroff
         fi
