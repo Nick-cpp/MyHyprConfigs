537c65cdf30faa0e3aa576ce2a6593df36c9079f
Mikola 2.0 <gnots.nik@gmail.com>
2026-01-15 17:58:19 +0500

    Update power-menu-artix.sh

--- scripts/power-menu-artix.sh
+++ scripts/power-menu-artix.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-choice=$(echo -e "Reboot\nShutdown" | wofi --dmenu --prompt="Power Menu" --height=120 --width=250)
+choice=$(echo -e "Reboot\nShutdown" | wofi --dmenu --prompt="Power Menu" --height=150 --width=250)
 
 case "$choice" in
     "Reboot")
-        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=100 --width=200)
+        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Reboot?" --height=150 --width=200)
         if [ "$confirm" = "Yes" ]; then
             pkill -15 -x hyprland
             loginctl reboot
         fi
         ;;
     "Shutdown")
-        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=100 --width=200)
+        confirm=$(echo -e "Yes\nNo" | wofi --dmenu --prompt="Shutdown?" --height=150 --width=200)
         if [ "$confirm" = "Yes" ]; then
             pkill -15 -x hyprland
             loginctl poweroff
