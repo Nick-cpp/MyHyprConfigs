d91a45d38930386eafe99d9abc5a2c7289d5a0fe
Mikola 2.0 <gnots.nik@gmail.com>
2026-02-07 20:27:34 +0500

    Enhance fish config with random greeting phrases
    Add random phrases to display on interactive shell startup.

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -10,6 +10,16 @@ alias wmconf="vim ~/.config/hypr/"
 alias plant="cbonsai"
 
 if status is-interactive
+    set phrases \
+        "Linux is not a kernel, it's a way of life." \
+        "Touch grass? I prefer touch /dev/random." \
+        "You should touch some grass."
+
+    set random_index (random 1 (count $phrases))
+
+    set_color cyan
+    echo -e "\e[3m$phrases[$random_index]\e[0m"
+    set_color normal
 end
 set -U fish_greeting
 function super
