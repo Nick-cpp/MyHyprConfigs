b36a7ddc23dc4f489f5d967f3884ee932bda4abb
Mikola 2.0 <gnots.nik@gmail.com>
2026-03-21 22:03:36 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,14 @@ doas chown -R root /etc/portage/package.use/
 echo "Installing needed packages..."
 sleep 2
 doas emerge --unmerge vim
-doas emerge gui-apps/foot sys-fs/udiskie media-sound/cmus gui-apps/mako app-arch/unzip gui-apps/hyprpicker media-fonts/noto-emoji media-fonts/noto media-fonts/noto-cjk app-misc/nwg-look media-gfx/gthumb x11-misc/spacefm gui-apps/waybar gui-apps/wofi media-fonts/fontawesome media-fonts/jetbrains-mono app-shells/fish media-fonts/dejavu app-misc/fastfetch app-misc/neofetch media-sound/pavucontrol gui-apps/hyprshot gui-apps/hyprlock
+doas emerge gui-apps/foot sys-fs/udiskie media-sound/cmus gui-apps/mako app-arch/unzip gui-apps/hyprpicker media-fonts/noto-emoji media-fonts/noto media-fonts/noto-cjk app-misc/nwg-look media-gfx/gthumb x11-misc/spacefm gui-apps/waybar gui-apps/wofi media-fonts/fontawesome media-fonts/jetbrains-mono app-shells/fish media-fonts/dejavu app-misc/fastfetch app-misc/neofetch media-sound/pavucontrol gui-apps/hyprshot gui-apps/hyprlock app-editors/vim
 
 doas mkdir -p /etc/iwd
 echo "[General]" | doas tee /etc/iwd/main.conf
 echo "EnableNetworkConfiguration=true" | doas tee -a /etc/iwd/main.conf
 echo "[Network]" | doas tee -a /etc/iwd/main.conf
 echo "NameResolvingService=resolvconf" | doas tee -a /etc/iwd/main.conf
+
 echo "nameserver 8.8.8.8" | doas tee /etc/iwd/resolv.conf
 echo "nameserver 8.8.4.4" | doas tee -a /etc/resolv.conf
 
