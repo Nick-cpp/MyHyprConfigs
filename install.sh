0f595b7b7ea45380ab7cf65a2e126753cf6d5b56
Mikola 2.0 <gnots.nik@gmail.com>
2026-03-21 22:33:00 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ sleep 2
 doas emerge --unmerge vim
 doas emerge gui-apps/foot sys-fs/udiskie media-sound/cmus gui-apps/mako app-arch/unzip gui-apps/hyprpicker media-fonts/noto-emoji media-fonts/noto media-fonts/noto-cjk app-misc/nwg-look media-gfx/gthumb x11-misc/pcmanfm gui-apps/waybar gui-apps/wofi media-fonts/fontawesome media-fonts/jetbrains-mono app-shells/fish media-fonts/dejavu app-misc/fastfetch app-misc/neofetch media-sound/pavucontrol gui-apps/hyprshot gui-apps/hyprlock app-editors/vim
 
+echo "Network configuration"
 doas mkdir -p /etc/iwd
 echo "[General]" | doas tee /etc/iwd/main.conf
 echo "EnableNetworkConfiguration=true" | doas tee -a /etc/iwd/main.conf
@@ -25,6 +26,8 @@ echo "NameResolvingService=resolvconf" | doas tee -a /etc/iwd/main.conf
 
 echo "nameserver 8.8.8.8" | doas tee /etc/iwd/resolv.conf
 echo "nameserver 8.8.4.4" | doas tee -a /etc/resolv.conf
+
+doas cp ~/MyHyprConfigs/impala /usr/bin/
 
 echo "Installing vim plug..."
 sleep 2
