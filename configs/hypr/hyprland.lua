66a7021808b99c939f93b9fa8f8a1fa67195db59
Mikola 2.0 <gnots.nik@gmail.com>
2026-06-27 19:34:10 +0500

    Update workspace rules for monitors in hyprland.lua

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -29,14 +29,15 @@ hl.monitor({
     scale    = "1",
 })
 
-hl.workspace_rule({workspace = 1, monitor="eDP-1"})
-hl.workspace_rule({workspace = 2, monitor="eDP-1"})
-hl.workspace_rule({workspace = 3, monitor="eDP-1"})
-hl.workspace_rule({workspace = 4, monitor="eDP-1"})
-hl.workspace_rule({workspace = 5, monitor="HDMI-A-1"})
-hl.workspace_rule({workspace = 6, monitor="HDMI-A-1"})
-hl.workspace_rule({workspace = 7, monitor="HDMI-A-1"})
-hl.workspace_rule({workspace = 8, monitor="HDMI-A-1"})
+hl.workspace_rule({ workspace = "1", monitor = "eDP-2", default = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-2", default = true })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-2", default = true })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-2", default = true })
+
+hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", default = true })
+hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", default = true })
+hl.workspace_rule({ workspace = "7", monitor = "HDMI-A-1", default = true })
+hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1", default = true })
 
 ---------------------
 ---- MY PROGRAMS ----
