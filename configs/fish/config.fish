510ba194291f00bc71ac600fbcdf339673c16fe3
Mikola 2.0 <gnots.nik@gmail.com>
2026-07-22 18:08:09 +0500

    Implement translate function in config.fish
    Added a translate function that uses Google Translate API.

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -17,7 +17,9 @@ end
 
 if status is-interactive
 end
+
 set -U fish_greeting
+
 function super
     su -c "$argv"
 end
@@ -31,4 +33,12 @@ end
 
 function on_exit --on-event fish_exit
     printf "\e[?25l"
+end
+
+function translate
+    curl -sA "Mozilla/5.0" \
+        -d "client=gtx" -d "sl=auto" -d "tl=$argv[1]" -d "dt=t" \
+        --data-urlencode "q=$argv[2]" \
+        "https://translate.googleapis.com/translate_a/single" \
+        | grep -o '"[^"]*"' | head -1 | tr -d '"'
 end
